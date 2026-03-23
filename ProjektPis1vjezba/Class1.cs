1b5ae2a2b5b9d712e97d65fa882ae18760ee0609
mmilin01 <lperic02@fesb.hr>
2026-03-23 19:46:02 +0100

    Izmjena u novoj klasi/dodana metoda

--- ProjektPis1vjezba/Class1.cs
+++ ProjektPis1vjezba/Class1.cs
@@ -1,12 +1,12 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace ProjektPis1vjezba
 {
     internal class Class1
     {
+        public void IspisiPoruku()
+        {
+            Console.WriteLine("Poruka iz nove klase");
+        }
     }
 }
